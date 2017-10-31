1d5a58a36ae03180fe4475cc795671caeaeae481
ohigesan <makotokoda82@gmail.com>
2017-10-31 17:18:10 +0900

    first commit

---
+++ main.php
@@ -0,0 +1,37 @@
+<?php
+require_once('quiz.class.php');
+$quizzes = new Quiz();
+
+$correct_num = 0;
+for ($i = 0; $i < $quizzes::NUMBERS_OF_QUIZZES; $i++ ) {
+   // 問題の表示
+   $current_quiz_num = $i + 1;
+   echo "Question. $current_quiz_num" . PHP_EOL;
+   echo "問題：" . $quizzes->getQuiz($i) . PHP_EOL;
+
+   // 選択肢の表示
+   $choices = $quizzes->getChoices($i);
+   $choice_num = 1;
+   foreach ($choices as $choice) {
+      echo "選択肢$choice_num. " . $choice . PHP_EOL;
+      $choice_num++;
+   }
+
+   // 回答の入力の受付
+   $user_answer = trim(fgets(STDIN));
+
+   // 答え合わせ
+   if ($user_answer === $quizzes->getAnswer($i)) {
+      $correct_num++;
+      echo "正解！！" . PHP_EOL;
+   } else {
+      echo "不正解！！" . PHP_EOL;
+   }
+
+   // 画面のクリア
+   sleep(2);
+   echo "\033[;H\033[2J";
+   
+   // 最終問題なら採点結果を表示
+}
+
---
+++ quiz.class.php
@@ -0,0 +1,31 @@
+<?php
+class Quiz
+{
+   const NUMBERS_OF_QUIZZES = 3;
+
+   private $quizzes = array(
+         array('question' => '1 + 1は？', 'answer' => 0, 'choice' => array(1, 2, 3)),
+         array('question' => '1 * 1は？', 'answer' => 1, 'choice' => array(0, 1, 2)),
+         array('question' => '1 - 1は？', 'answer' => 1, 'choice' => array(-1, 0, 1)),
+      );
+
+   public function __construct()
+   {
+      shuffle($this->quizzes);
+   }
+
+   public function getQuiz($num)
+   {
+      return $this->quizzes[$num]['question'];
+   }
+
+   public function getAnswer($num)
+   {
+      return $this->quizzes[$num]['answer'];
+   }
+
+   public function getChoices($num)
+   {
+      return $this->quizzes[$num]['choice'];
+   }
+}
